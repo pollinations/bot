d0768740c9c7e0bba3fffe71c139e0439e49173a
Alexander Reiling <alexanderreiling@googlemail.com>
2022-08-11 00:34:07 +0200

    Incorporate latest changes from main branch

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -1,7 +1,9 @@
 import 'dotenv/config';
-import discordjs, { GuildTextBasedChannel } from 'discord.js';
+import discordjs, { GuildTextBasedChannel, Message } from 'discord.js';
 import { runModelGenerator } from '@pollinations/ipfs/awsPollenRunner.js';
 import lodash from 'lodash';
+import fetch from 'node-fetch';
+import fs from 'fs';
 
 const { Client, GatewayIntentBits, EmbedBuilder } = discordjs;
 
@@ -24,29 +26,31 @@ client.on('ready', () => {
 
 const CHANNEL_CONFIG = {
   'dalle-mini': {
-    model:
-      '614871946825.dkr.ecr.us-east-1.amazonaws.com/voodoohop/dalle-playground',
+    model: '614871946825.dkr.ecr.us-east-1.amazonaws.com/voodoohop/dalle-playground',
     promptField: 'prompt',
     channelId: '999295739727466528'
   },
   'latent-diffusion': {
-    model:
-      '614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-frontpage',
+    model: '614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-frontpage',
     promptField: 'Prompt',
     channelId: '999296010025173012'
   },
   'majesty-diffusion': {
-    model:
-      '614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/majesty-diffusion-cog',
+    model: '614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/majesty-diffusion-cog',
     promptField: 'Prompt',
     channelId: '999295785621540914',
     numImages: 1
   },
   'disco-diffusion': {
-    model:
-      '614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/disco-diffusion',
+    model: '614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/disco-diffusion',
     promptField: 'prompt',
     channelId: '1003013847562592306',
+    numImages: 1
+  },
+  photo3d: {
+    model: '614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/adampi',
+    promptField: 'image',
+    channelId: '1007030609060823082',
     numImages: 1
   }
 };
@@ -55,20 +59,24 @@ type ChannelName = keyof typeof CHANNEL_CONFIG;
 const channelNames = Object.keys(CHANNEL_CONFIG);
 
 const clickableChannelIDs = channelNames
-  .map(
-    (channelName) =>
-      `<#${CHANNEL_CONFIG[channelName as ChannelName].channelId}>`
-  )
+  .map((channelName) => `<#${CHANNEL_CONFIG[channelName as ChannelName].channelId}>`)
   .join(', ');
 
 client.on('messageCreate', async (dMessage) => {
   // return if message is by a bot
-
   if (dMessage.author.bot) return;
+
   const channel = dMessage.channel as GuildTextBasedChannel;
   const channelNameUnvalidated = (channel as GuildTextBasedChannel).name;
 
-  if (channelNames.includes(channelNameUnvalidated)) return;
+  if (!channelNames.includes(channelNameUnvalidated)) {
+    await dMessage.react('🚫');
+    await dMessage.reply(
+      'This channel is not supported. Please use one of the following channels to send your prompt: ' +
+        clickableChannelIDs
+    );
+    return;
+  }
 
   const channelName = channelNameUnvalidated as ChannelName;
   const config = CHANNEL_CONFIG[channelName];
@@ -77,15 +85,6 @@ client.on('messageCreate', async (dMessage) => {
 
   // return if message is not a mention of the bot
   if (dMessage.content.indexOf(botIDString) === -1) return;
-
-  if (!channelNames.includes(channelName)) {
-    await dMessage.react('🚫');
-    await dMessage.reply(
-      'This channel is not supported. Please use one of the following channels to send your prompt: ' +
-        clickableChannelIDs
-    );
-    return;
-  }
 
   const { model, promptField } = CHANNEL_CONFIG[channelName];
   const prettyModelName = modelNameDescription(model);
@@ -96,12 +95,14 @@ client.on('messageCreate', async (dMessage) => {
 
   dMessage.react('🐝');
 
-  const message = dMessage.content.replace(botIDString, '');
+  // check if message has attachments
+  const attachment = checkAttachment(dMessage);
 
-  const messageRef = await dMessage.reply(
-    `Creating: **${message}** using model: **${prettyModelName}**.`
-  );
-  const editReply = lodash.throttle((arg) => messageRef.edit(arg), 10000);
+  // message is either the attachment or the message interpreted as the text prompt (without the bot name)
+  const message = attachment || dMessage.content.replace(botIDString, '');
+
+  const messageRef = await dMessage.reply(`Creating: **${message}** using model: **${prettyModelName}**.`);
+  const editReply = lodash.throttle(messageRef.edit.bind(messageRef), 10000);
 
   console.log('running model generator', model, { [promptField]: message });
   const results = runModelGenerator(
@@ -119,15 +120,28 @@ client.on('messageCreate', async (dMessage) => {
 
     //@ts-ignore
     const images = getImages(output).slice(0, config.numImages || Infinity);
+    console.log('got images', images);
 
-    // inside a command, event listener, etc.
-    const embeds = images.map(([_filename, image]) =>
-      createEmbed(prettyModelName, message, image, contentID)
+    const files = await Promise.all(
+      images
+        .filter(([filename, _url]) => filename.endsWith('.mp4'))
+        .map(async ([filename, url]) => {
+          console.log('fetching url', url);
+          const response = await fetch(url);
+          const buffer = await response.buffer();
+          // write to local filesystem
+          const filePath = `/tmp/${filename}`;
+          fs.writeFileSync(filePath, buffer);
+          console.log('wrote file', filePath);
+          return filePath;
+        })
     );
 
-    await editReply({
-      embeds
-    });
+    // inside a command, event listener, etc.
+    const embeds = images.map(([_filename, image]) => createEmbed(prettyModelName, message, image, contentID));
+
+    console.log('calling editReply', { embeds, files });
+    await editReply({ embeds, files });
   }
 });
 
@@ -143,12 +157,16 @@ const modelNameDescription = (modelName: string) =>
     .replaceAll('-', ' ')
     .replace(/\b\w/g, (l) => l.toUpperCase());
 
-function createEmbed(
-  modelNameHumanReadable: string,
-  messageWithoutBotName: string,
-  image: string,
-  contentID: string
-) {
+function checkAttachment(dMessage: Message) {
+  if (dMessage.attachments.size > 0) {
+    // get attachment image url
+    const { url } = dMessage.attachments.first()!;
+    console.log('got attachment with url', url);
+    return url;
+  }
+  return null;
+}
+function createEmbed(modelNameHumanReadable: string, messageWithoutBotName: string, image: string, contentID: string) {
   return new EmbedBuilder()
     .setDescription(`Model: **${modelNameHumanReadable}**`)
     .setTitle(messageWithoutBotName.slice(0, 250))
@@ -161,7 +179,7 @@ function getImages(output: any) {
 
   const images = outputEntries.filter(
     ([filename, url]) =>
-      (filename.endsWith('.png') || filename.endsWith('.jpg')) && url.length > 0
+      (filename.endsWith('.png') || filename.endsWith('.jpg') || filename.endsWith('.mp4')) && url.length > 0
   );
 
   return lodash.reverse(images.slice(-4));
