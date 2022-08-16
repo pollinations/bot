c94922882d678c9747d9d77693eda573ca37c860
Thomas Haferlach <t.haferlach@gmail.com>
2022-08-16 21:11:36 +0200

    add retrieval diffusion

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -52,6 +52,12 @@ const CHANNEL_CONFIG = {
     promptField: 'image',
     channelId: '1007030609060823082',
     numImages: 1
+  },
+  'retrieval-diffusion': {
+    model: '614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m',
+    promptField: 'prompts',
+    channelId: '1009168983578124449',
+    numImages: 1
   }
 };
 type ChannelName = keyof typeof CHANNEL_CONFIG;
