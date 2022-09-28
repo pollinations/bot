bcd0343077f1da1fdf29cc1dbbb747fc8be9b187
Alexander Reiling <alexanderreiling@googlemail.com>
2022-09-28 19:48:49 +0200

    Update logger

--- src/discord/logger.ts
+++ src/discord/logger.ts
@@ -1,19 +1,21 @@
 import { Level, pino, StreamEntry } from 'pino';
 const LOGS_DIR = 'logs';
-const devStreams: StreamEntry[] = [
-  { level: 'debug', stream: process.stdout },
-  { level: 'info', stream: pino.destination(`${LOGS_DIR}/combined.log`) },
-  { level: 'error', stream: pino.destination(`${LOGS_DIR}/error.log`) }
-];
-const prodStreams: StreamEntry[] = [
-  { level: (process.env['PINO_LOG_LEVEL'] as Level) || 'info', stream: process.stdout },
-  { level: 'error', stream: process.stderr }
-];
 const inDev = process.env['NODE_ENV'] === 'development';
+
+const streams: StreamEntry[] = inDev
+  ? [
+      { level: 'debug', stream: process.stdout },
+      { level: 'info', stream: pino.destination(`${LOGS_DIR}/combined.log`) },
+      { level: 'error', stream: pino.destination(`${LOGS_DIR}/error.log`) }
+    ]
+  : [
+      { level: (process.env['PINO_LOG_LEVEL'] as Level) || 'info', stream: process.stdout },
+      { level: 'error', stream: process.stderr }
+    ];
 const logger = pino(
   {
     level: process.env['PINO_LOG_LEVEL'] || 'info'
   },
-  pino.multistream(inDev ? devStreams : prodStreams)
+  pino.multistream(streams)
 );
 export default logger;
